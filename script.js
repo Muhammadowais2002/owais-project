86590a3a41bf8e9871eb0142b2c6a161c1f46502
Muhammadowais2002 <owais111champ@gmail.com>
2024-11-04 10:33:52 +0530

    Add files via upload
    ok

---
+++ script.js
@@ -0,0 +1,80 @@
+function open_dropdown1(){
+    document.getElementById("nav-extension1").style.height="350px"
+    document.getElementById("navitem1").style.background="rgb(37, 36, 36)"
+    document.getElementById("chevron1").classList.remove("fa-rotate-180")
+}
+function close_dropdown1(){
+    document.getElementById("nav-extension1").style.height="0";
+    document.getElementById("navitem1").style.background="black"
+    document.getElementById("chevron1").classList.add("fa-rotate-180") 
+}
+function open_dropdown2(){
+    document.getElementById("nav-extension2").style.height="400px"
+    document.getElementById("navitem2").style.background="rgb(37, 36, 36)"
+    document.getElementById("chevron2").classList.remove("fa-rotate-180")
+}
+function close_dropdown2(){
+    document.getElementById("nav-extension2").style.height="0";
+    document.getElementById("navitem2").style.background="black"
+    document.getElementById("chevron2").classList.add("fa-rotate-180") 
+}
+function open_dropdown3(){
+    document.getElementById("nav-extension3").style.height="400px"
+    document.getElementById("navitem3").style.background="rgb(37, 36, 36)"
+    document.getElementById("chevron3").classList.remove("fa-rotate-180")
+}
+function close_dropdown3(){
+    document.getElementById("nav-extension3").style.height="0";
+    document.getElementById("navitem3").style.background="black"
+    document.getElementById("chevron3").classList.add("fa-rotate-180") 
+}
+function open_dropdown4(){
+    document.getElementById("nav-extension4").style.height="370px"
+    document.getElementById("navitem4").style.background="rgb(37, 36, 36)"
+    document.getElementById("chevron4").classList.remove("fa-rotate-180")
+}
+function close_dropdown4(){
+    document.getElementById("nav-extension4").style.height="0";
+    document.getElementById("navitem4").style.background="black"
+    document.getElementById("chevron4").classList.add("fa-rotate-180") 
+}
+function open_dropdown5(){
+    document.getElementById("nav-extension5").style.height="250px"
+    document.getElementById("navitem5").style.background="rgb(37, 36, 36)"
+    document.getElementById("chevron5").classList.remove("fa-rotate-180")
+}
+function close_dropdown5(){
+    document.getElementById("nav-extension5").style.height="0";
+    document.getElementById("navitem5").style.background="black"
+    document.getElementById("chevron5").classList.add("fa-rotate-180") 
+}
+function open_footer_extension1(){
+    document.getElementById("chevron6").classList.remove("fa-rotate-180");
+    document.getElementById("footer-extension1").style.height="fit-content";
+}
+function close_footer_extension1(){
+    document.getElementById("chevron6").classList.add("fa-rotate-180");
+    document.getElementById("footer-extension1").style.height="0";
+}
+function open_footer_extension2(){
+    document.getElementById("chevron7").classList.remove("fa-rotate-180");
+    document.getElementById("footer-extension2").style.height="fit-content";
+}
+function close_footer_extension2(){
+    document.getElementById("chevron7").classList.add("fa-rotate-180");
+    document.getElementById("footer-extension2").style.height="0";
+}
+function open_footer_extension3(){
+    document.getElementById("chevron8").classList.remove("fa-rotate-180");
+    document.getElementById("footer-extension3").style.height="fit-content";
+}
+function close_footer_extension3(){
+    document.getElementById("chevron8").classList.add("fa-rotate-180");
+    document.getElementById("footer-extension3").style.height="0";
+}
+function open_ham_menu(){
+    document.getElementById("ham-box").style.width="400px";
+}
+function close_ham_menu(){
+    document.getElementById("ham-box").style.width="0";
+}
